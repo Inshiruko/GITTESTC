ce2c95fc055873380c7901982694dff266e9e855
Inshiruko <artur.tymchuk.w@gmail.com>
2021-08-07 14:38:20 +0300

    Добавьте файлы проекта.

---
+++ Test/Test.cpp
@@ -0,0 +1,25 @@
+﻿
+// Git Test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int func(int a, int b)
+{
+    return a * b;
+}
+int main()
+{
+    int a;
+    int b;
+    int res;
+    std::cout << "Vedite а" << endl;
+    std::cin >> a;
+    std::cout << "vedite b" << endl;
+    cin >> b;
+    res = func(a, b);
+
+    cout << res;
+}
+
